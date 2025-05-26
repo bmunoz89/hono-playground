1893610fc7c616b053201291ebf8f7cca13e9785
Boris Muñoz <bmunoz89@users.noreply.github.com>
2025-05-25 21:25:34 -0400

    WIP

--- app.ts
+++ app.ts
@@ -1,11 +1,32 @@
 import env from "@env";
 import mainRouter from "@routes/main.router";
 import testRouter from "@routes/test.router";
+import type { Env } from "@types";
+import { AsyncLocalStorage } from "async_hooks";
 import type { Serve } from "bun";
+import { randomUUID } from "crypto";
 import { Hono } from "hono";
+import { contextStorage } from "hono/context-storage";
 import { showRoutes } from "hono/dev";
 
-const app = new Hono()
+export const asyncLocalStorage = new AsyncLocalStorage<{
+  message: string;
+}>();
+
+const app = new Hono<Env>()
+  .use(contextStorage())
+  .use(async (c, next) => {
+    c.set("message", randomUUID());
+    await asyncLocalStorage.run({ message: randomUUID() }, () => {
+      return next();
+    });
+  })
+  .use(async (c, next) => {
+    const store = asyncLocalStorage.getStore();
+    console.log(c.req.path, "builtin middleware", c.get("message"));
+    console.log(c.req.path, "native middleware", store!.message);
+    await next();
+  })
   .basePath("/api")
   .route("/main", mainRouter)
   .route("/test", testRouter);
--- routes/main.router.ts
+++ routes/main.router.ts
@@ -1,13 +1,18 @@
+import { asyncLocalStorage } from "@app";
 import { zValidator } from "@hono/zod-validator";
 import { jsonSchema, paramSchema } from "@routes/main.schema";
+import type { Env } from "@types";
 import { Hono } from "hono";
 
-export default new Hono()
+export default new Hono<Env>()
   .post("/", zValidator("json", jsonSchema), (c) => {
     const json = c.req.valid("json");
     return c.json(json);
   })
   .get("/:bar/:baz", zValidator("param", paramSchema), (c) => {
     const param = c.req.valid("param");
+    const store = asyncLocalStorage.getStore();
+    console.log(c.req.path, "native endpoint", store!.message);
+    console.log(c.req.path, "builtin endpoint", c.get("message"));
     return c.json(param);
   });
