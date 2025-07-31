8975eb9de011cbed947fb9da81ada78d1f4daa59
Boris Muñoz <bmunoz89@users.noreply.github.com>
2025-07-31 21:51:34 +0200

    Implement helm configurations to work with a redis master o sentinel

--- src/cache/redis.ts
+++ src/cache/redis.ts
@@ -1,9 +1,47 @@
 import env from "@/env";
-import Redis from "ioredis";
+import { redis } from "bun";
+import Redis, { Cluster } from "ioredis";
 
-const redis = new Redis({
-  host: env.REDIS_HOST,
-  port: env.REDIS_PORT,
-});
+let redis: Redis | Cluster;
+switch (env.REDIS_MODE) {
+  case "standard":
+    redis = new Redis({
+      host: env.REDIS_HOST,
+      port: env.REDIS_PORT,
+      username: env.REDIS_USERNAME,
+      password: env.REDIS_PASSWORD,
+      enableAutoPipelining: true,
+    });
+    break;
+  case "sentinel":
+    redis = new Redis({
+      name: env.REDIS_MASTER_NAME,
+      sentinels: env.REDIS_SENTINEL_NODES,
+      username: env.REDIS_USERNAME,
+      password: env.REDIS_PASSWORD,
+      sentinelUsername: env.REDIS_USERNAME,
+      sentinelPassword: env.REDIS_PASSWORD,
+      enableAutoPipelining: true,
+    });
+    break;
+  case "cluster":
+    // NOT WORKING - Getting error when connecting to pods
+    // Unhandled error event: Error: Failed to refresh slots cache
+    redis = new Cluster(env.REDIS_CLUSTER_NODES, {
+      // dnsLookup: (address, callback) => callback(null, address),
+      redisOptions: {
+        // tls: {},
+        username: env.REDIS_USERNAME,
+        password: env.REDIS_PASSWORD,
+      },
+      scaleReads: "slave",
+      enableAutoPipelining: true,
+    });
+    break;
+}
+
+export function isCluster(redis: Redis | Cluster): redis is Cluster {
+  return redis.isCluster;
+}
 
 export default redis;
--- src/env.ts
+++ src/env.ts
@@ -1,24 +1,65 @@
 import { z } from "zod";
 
-const envSchema = z
-  .object({
-    TZ: z.string().default("UTC"),
-    PORT: z
-      .string()
-      .or(z.number())
-      .default(3000)
-      .pipe(z.coerce.number().int().min(0).max(65535)),
-    NODE_ENV: z
-      .enum(["development", "production", "test"])
-      .default("development"),
-    REDIS_HOST: z.string().default("127.0.0.1"),
-    REDIS_PORT: z
-      .string()
-      .or(z.number())
-      .default(6379)
-      .pipe(z.coerce.number().int().min(0).max(65535)),
-  })
-  .readonly();
+const baseSchema = z.object({
+  TZ: z.string().default("UTC"),
+  PORT: z
+    .string()
+    .or(z.number())
+    .default(3000)
+    .pipe(z.coerce.number().int().min(0).max(65535)),
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  REDIS_RETRY_TIMEOUT: z.number().default(1000),
+  REDIS_USERNAME: z.string().default("default"),
+  REDIS_PASSWORD: z.string().optional(),
+});
+
+const redisNodesSchema = z
+  .string()
+  .transform((value) => JSON.parse(value))
+  .pipe(
+    z
+      .object({
+        host: z.string(),
+        port: z
+          .string()
+          .or(z.number())
+          .default(26379)
+          .pipe(z.coerce.number().int().min(0).max(65535)),
+      })
+      .array(),
+  );
+
+const envSchema = z.discriminatedUnion("REDIS_MODE", [
+  z
+    .object({
+      REDIS_MODE: z.literal("standard"),
+      REDIS_HOST: z.string().default("redis"),
+      REDIS_PORT: z
+        .string()
+        .or(z.number())
+        .default(6379)
+        .pipe(z.coerce.number().int().min(0).max(65535)),
+    })
+    .merge(baseSchema),
+  z
+    .object({
+      REDIS_MODE: z.literal("sentinel"),
+      REDIS_SENTINEL_NODES: redisNodesSchema,
+      REDIS_MASTER_NAME: z
+        .string()
+        .default("mymaster")
+        .describe("Can be set/obtained in the chart sentinel.masterSet field"),
+    })
+    .merge(baseSchema),
+  z
+    .object({
+      REDIS_MODE: z.literal("cluster"),
+      REDIS_CLUSTER_NODES: redisNodesSchema,
+    })
+    .merge(baseSchema),
+]);
 
 const parsedEnv = envSchema.safeParse(process.env);
 if (!parsedEnv.success) {
